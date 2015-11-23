73a178a823835288cc91f51f255a0f47023df12f
Gabriel Gironda <gabriel@thegroundwork.com>
2015-11-23 18:17:18 -0500

    Update word list

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,46 +1,56 @@
+var replacements = [
+    ['Cryptography', 'Mathematics'],
+    ['cryptography', 'mathematics'],
+    ['Crypto', 'Math'],
+    ['crypto', 'math'],
+    ['Encryption', 'Math problems'],
+    ['encryption', 'math problems'],
+    ['Encrypted', 'Mathed'],
+    ['encrypted', 'mathed'],
+    ['Cryptographer', 'Math-Liker'],
+    ['cryptographer', 'math-liker'],
+];
+
+for (i = 0; i < replacements.length; i++) {
+    replacements[i][0] = new RegExp('\\b' + replacements[i][0] + '\\b', 'g');
+}
+
+function walk(node)
+{
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+
+    var child, next;
+
+    switch (node.nodeType)
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child)
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode)
+{
+    var v = textNode.nodeValue;
+
+    for (i = 0; i < replacements.length; i++) {
+        v = v.replace(replacements[i][0], replacements[i][1]);
+    }
+
+    textNode.nodeValue = v;
+}
+
 walk(document.body);
-
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bCryptography\b/g, "Mathematics");
-	v = v.replace(/\bcryptography\b/g, "mathematics");
-	v = v.replace(/\bCrypto\b/g, "Math");
-	v = v.replace(/\bcrypto\b/g, "math");
-	v = v.replace(/\bEncryption\b/g, "Math problems");
-	v = v.replace(/\bencryption\b/g, "math problems");
-	v = v.replace(/\bEncrypted\b/g, "Math-using");
-	v = v.replace(/\bencrypted\b/g, "math-using");
-
-	textNode.nodeValue = v;
-}
-
-
